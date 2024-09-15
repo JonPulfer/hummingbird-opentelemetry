0e58331ce35520db9fe936bb2e430d7190499600
Jonathan Pulfer <JonPulfer@users.noreply.github.com>
2024-09-15 13:35:18 +0100

    start planning components

--- Sources/hummingbird-opentelemetry/Middleware.swift
+++ Sources/hummingbird-opentelemetry/Middleware.swift
@@ -9,6 +9,16 @@ import Foundation
 import Hummingbird
 
 
-final class OpenTelemetryMiddleware: MiddlewareProtocol {
+final class OpenTelemetryMiddleware<Context: RequestContext>: RouterMiddleware {
 	
+	// TODO: Hold an injected connection to the OpenTelemetry collector
+	
+	// TODO: Create an init function to receive the injected connection
+	
+	public func handle(_ request: Request, context: Context, next: (Request, Context) async throws -> Response) async throws -> Response {
+			// log request URI
+			
+			// pass request onto next middleware or the router and return response
+			return try await next(request, context)
+		}
 }
--- Sources/hummingbird-opentelemetry/hummingbird_opentelemetry.swift
+++ Sources/hummingbird-opentelemetry/hummingbird_opentelemetry.swift
@@ -1,2 +1,4 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+// TODO: Create a struct to act as an OpenTelemetry client
